084e4684f36cfd114c3cab5c98aaf029facc0beb
ZezhongWang <904192625@qq.com>
2022-03-16 23:05:37 +0200

    Update power up

--- Assets/Scripts/Level/LevelElements/PowerUp.cs
+++ Assets/Scripts/Level/LevelElements/PowerUp.cs
@@ -5,8 +5,6 @@ using UnityEngine;
 public class PowerUp : MonoBehaviour
 {
 
-    [SerializeField] private float _rotateSpeed = 1.0f;
-
     private ObjectPoolUnit _poolUnit;
     private GameObject _playerObj;
 
@@ -17,17 +15,14 @@ public class PowerUp : MonoBehaviour
         _playerObj = GameObject.FindWithTag(Tags.PLAYER_TAG);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Rotate(new Vector3(0, _rotateSpeed * Time.deltaTime, 0), Space.World);
-    }
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.tag == Tags.PLAYER_TAG)
+        if (other.gameObject.tag == Tags.PLAYER_TAG)
+        {
             _playerObj.GetComponent<PlayerMovement>().OnPowerUpCollected();
-        gameObject.SetActive(false);    // pooled management
-        _poolUnit.Deactivate();
+            gameObject.SetActive(false);    // pooled management
+            _poolUnit.Deactivate();
+        }
     }
 }
