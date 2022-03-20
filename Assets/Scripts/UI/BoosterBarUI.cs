c496ea9cf4b6e7388db2f44f969646f5fe45a1ee
Aliinsti <aliinsti@protonmail.com>
2022-03-21 00:21:55 +0100

    Speed bar and booster bar UI polished

--- Assets/Scripts/UI/BoosterBarUI.cs
+++ Assets/Scripts/UI/BoosterBarUI.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BoosterBarUI : MonoBehaviour
 {
@@ -8,17 +9,18 @@ public class BoosterBarUI : MonoBehaviour
     private PlayerMovement playerMovement;
 
     [SerializeField]
-    private BarUI boosterBar;
+    private Image[] boosterBarCells;
 
     // Start is called before the first frame update
     void Start()
     {
-        boosterBar = GetComponent<BarUI>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        boosterBar.SetValue(playerMovement.CurrentBoostAmount / playerMovement.MaxBoostAmount);
+        int enabledCellAmount = Mathf.FloorToInt(Mathf.Min(playerMovement.CurrentBoostAmount, boosterBarCells.Length));
+        for (int i = boosterBarCells.Length - 1; i >= 0; --i)
+            boosterBarCells[i].enabled = i < enabledCellAmount;
     }
 }
