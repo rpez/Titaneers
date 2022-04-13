11a7e4b3a22cf613003947bffead016949eeec56
GeeNoiseworks <mikko.g.saarinen@gmail.com>
2022-04-13 15:24:28 +0300

    Ambience sound loop and outdoor reverb
    Add ambience sfx and a outdoor reverb to the scene

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,6 +13,7 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID PLAY_2D_AMBIENCE_LOOP = 706595652U;
         static const AkUniqueID PLAY_COMBAT_MUSIC = 3155474038U;
         static const AkUniqueID PLAY_PLAYER_BOOSTER = 142245832U;
         static const AkUniqueID PLAY_PLAYER_FS_RUN_L = 3625340534U;
@@ -31,6 +32,7 @@ namespace AK
         static const AkUniqueID PLAY_TITAN_TRAMPLE_FOOT = 1282535387U;
         static const AkUniqueID PLAY_TITAN_TRAMPLE_VOC = 3060992713U;
         static const AkUniqueID PLAY_TITAN_VOC_GENERIC = 3131183803U;
+        static const AkUniqueID STOP_2D_AMBIENCE_LOOP = 1752755706U;
         static const AkUniqueID STOP_COMBAT_MUSIC = 970928956U;
         static const AkUniqueID STOP_TITAN_MISSILE_LOOP = 930574960U;
     } // namespace EVENTS
