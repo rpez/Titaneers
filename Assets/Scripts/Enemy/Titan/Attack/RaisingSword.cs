66289140ed743d26182fc679d5c85af31a2a2ae4
LingPeiYu <48287204+LingPeiYu@users.noreply.github.com>
2022-04-19 02:29:46 +0300

    Update Titan Sword Attack

---
+++ Assets/Scripts/Enemy/Titan/Attack/RaisingSword.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaisingSword : MonoBehaviour
+{
+    [SerializeField]
+    private float _lowestY;
+    [SerializeField]
+    private float _highestY;
+    [SerializeField]
+    private AnimationCurve _raisingCurve;
+
+    [SerializeField]
+    private float _raisingTime;
+
+    private float _timer;
+
+    private void Start()
+    {
+        Init();
+    }
+
+    private void Update()
+    {
+        if (_timer < _raisingTime)
+        {
+            _timer += Time.deltaTime;
+            float y = Mathf.Lerp(_lowestY, _highestY, _raisingCurve.Evaluate(_timer / _raisingTime));
+            transform.position = new Vector3(transform.position.x, y, transform.position.z);
+        }
+    }
+
+    public void Init()
+    {
+        _timer = 0f;
+    }
+}
