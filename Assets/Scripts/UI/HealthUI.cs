ac0dcde497843a14180061c58645bd7b8cee54fd
Aliinsti <aliinsti@protonmail.com>
2022-05-02 14:38:08 +0200

    User health and low health hue effect v1.0

--- Assets/Scripts/UI/HealthUI.cs
+++ Assets/Scripts/UI/HealthUI.cs
@@ -7,11 +7,13 @@ public class HealthUI : MonoBehaviour
 {
     [SerializeField]
     private BarUI healthBar;
-
+    [SerializeField]
+    private UI mainUI;
     [SerializeField]
     private Health health;
     [SerializeField]
     private GameObject bg;
+
     private void Start()
     {
     }
@@ -19,9 +21,17 @@ public class HealthUI : MonoBehaviour
     private void Update()
     {
         float currentHealthPoint = health.CurrentHealthPoint;
-        healthBar.SetValue(currentHealthPoint > 0f ? currentHealthPoint / health.MaxHealthPoint: 0f);
-        if (currentHealthPoint <= 0)
-            bg.SetActive(false);
-        else bg.SetActive(true);
+        float healthBarValue = currentHealthPoint > 0f ? currentHealthPoint / health.MaxHealthPoint : 0f;
+        healthBar.SetValue(healthBarValue);
+
+        if (mainUI)
+        {
+            mainUI.ChangeLowHealthEffect(Mathf.Pow(1 - healthBarValue, 3f));
+        } else
+        {
+            if (currentHealthPoint <= 0)
+                bg.SetActive(false);
+            else bg.SetActive(true);
+        }
     }
 }
